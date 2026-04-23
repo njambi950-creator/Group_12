5456bf92a4eaf9a8fe2340609575e3093107a427
Njambi <njambingugi950@gmail.com>
2026-04-23 10:47:56 +0300

    Refactor number sorting logic and enhance comments for clarity

--- Duncan.js
+++ Duncan.js
@@ -1,6 +1,24 @@
 //Assignment 1
+//To create a constant list of numbers that wont change
 const numbers = [12, 45, 7, 22, 89, 34, 11, 6, 90, 53, 2, 17, 8];
 
-let evenNumbers = []
+//the numbers which will change
+let evenNumbers = [];
 let oddNumbers = [];
 
+//create loop to sort through the even and odd numbers
+for (let i = 0; i < numbers.length; i++) {
+  let currentNumber = numbers[i];
+
+  //Check if the numbers are even
+  if (currentNumber % 2 === 0) {
+    evenNumbers.push(currentNumber);
+  } else {
+    oddNumbers.push(currentNumber);
+  }
+}
+//Display even numbers
+console.log("Even Numbers:", evenNumbers);
+
+//Display odd numbers
+console.log("Odd Numbers:", oddNumbers);
