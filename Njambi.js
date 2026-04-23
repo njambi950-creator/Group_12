d73c86837d9635b2978e53d8c495d830e063250f
Njambi <njambingugi950@gmail.com>
2026-04-23 12:59:30 +0300

    Refactor prime number checking logic and improve output formatting; add README file

--- Njambi.js
+++ Njambi.js
@@ -1,42 +1,31 @@
-// Array of numbers we want to check
-const number = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-// Diesel price
 const dieselPrice = 196.63;
 
 // Function to check if a number is prime
 function isPrime(num) {
-  // numbers less than 2 are not prime numbers
-  if (num < 2) {
-    return false;
-  }
 
-  // loop to check if numbers has divisors
-  for (let i = 2; i < num; i++) {
-    //if divisible by any number other than one and itself
-    if (num % i === 0) {
-      return false; //not a prime number
-    }
-  }
-  // if no divisors found, it is a prime number
-  return true;
+if (num < 2) return false;
+
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
 }
 
-//loop through each number in the array
-for (let i = 0; i < number.length; i++) {
-  // get current number from array
-  let currentNumber = number[i];
-
-  // check if number is prime
-  if (isPrime(currentNumber)) {
-    // if prime, print the number
-    console.log(currentNumber + "is prime");
-  } else {
-    // if not prime, print diesel price
-    console.log(
-      currentNumber +
-        "is NOT Prime  Diesel price in kenya is KES" +
-        dieselPrice,
-    );
-  }
+return true;
 }
+
+// Loop through array
+for (let i = 0; i < numbers.length; i++) {
+
+let currentNumber = numbers[i];
+
+// If prime → print number only
+if (isPrime(currentNumber)) {
+    console.log(currentNumber);
+
+// If NOT prime → print diesel price only
+} else {
+    console.log(dieselPrice);
+}
+}
+
